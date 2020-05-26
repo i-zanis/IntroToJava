462f65ba4e7ddfd6fc3488d000cce82f9c23f033
i-zanis <55964867+i-zanis@users.noreply.github.com>
2020-05-26 02:06:55 +0100

    new exerc
    new exerc

---
+++ src/E3_19.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class E3_19 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter 3 numbers: ");
+        int A = input.nextInt();
+        int B = input.nextInt();
+        int C = input.nextInt();
+
+        int P = A + B + C;
+
+        if (A + B > C && A + C > B && B + C > A) {
+            System.out.println("The perimeter is valid. Perimeter: " + P + ".");
+        } else {
+            System.out.println("The perimeter is invalid.");
+        }
+    }
+
+}
+
+/**
+ * (Compute the perimeter of a triangle) Write a program that reads three edges for a triangle and computes the perimeter if the input is valid. Otherwise, display that the input is invalid. The input is valid if the sum of every pair of two edges is greater than the remaining edge.
+ */
