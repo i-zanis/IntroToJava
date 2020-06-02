d2b69a0fce56fd7640ceed7f372d30019b4eb633
i-zanis <55964867+i-zanis@users.noreply.github.com>
2020-06-03 00:15:36 +0100

    0015

--- src/E4_10.java
+++ src/E4_10.java
@@ -2,17 +2,86 @@ import java.util.Scanner;
 
 public class E4_10 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        long binaryNumber, decimalNumber = 0, j = 1, remainder;
-        System.out.print("Input a binary number: ");
-        binaryNumber = sc.nextLong();
+        String set1 =
+                " 1  3  5  7\n" +
+                        " 9 11 13 15\n" +
+                        "17 19 21 23\n" +
+                        "25 27 29 31";
 
-        while (binaryNumber != 0) {
-            remainder = binaryNumber % 10;
-            decimalNumber = decimalNumber + remainder * j;
-            j = j * 2;
-            binaryNumber = binaryNumber / 10;
-        }
-        System.out.println("Decimal Number: " + decimalNumber);
+        String set2 =
+                " 2  3  6  7\n" +
+                        "10 11 14 15\n" +
+                        "18 19 22 23\n" +
+                        "26 27 30 31";
+
+        String set3 =
+                " 4  5  6  7\n" +
+                        "12 13 14 15\n" +
+                        "20 21 22 23\n" +
+                        "28 29 30 31";
+
+        String set4 =
+                " 8  9 10 11\n" +
+                        "12 13 14 15\n" +
+                        "24 25 26 27\n" +
+                        "28 29 30 31";
+
+        String set5 =
+                "16 17 18 19\n" +
+                        "20 21 22 23\n" +
+                        "24 25 26 27\n" +
+                        "28 29 30 31";
+
+        int day = 0;
+
+        // Create a Scanner
+        Scanner input = new Scanner(System.in);
+
+        // Prompt the user to answer questions
+        System.out.print("Is your birthday in Set1?\n");
+        System.out.print(set1);
+        System.out.print("\nEnter 0 for No and 1 for Yes: ");
+        char answer = input.next().charAt(0);
+
+        if (answer == 'Y')
+            day += 1;
+
+        // Prompt the user to answer questions
+        System.out.print("\nIs your birthday in Set2?\n");
+        System.out.print(set2);
+        System.out.print("\nEnter 0 for No and 1 for Yes: ");
+        answer = input.next().charAt(0);
+
+        if (answer == 'Y')
+            day += 2;
+
+        // Prompt the user to answer questions
+        System.out.print("Is your birthday in Set3?\n");
+        System.out.print(set3);
+        System.out.print("\nEnter 0 for No and 1 for Yes: ");
+        answer = input.next().charAt(0);
+
+        if (answer == 'Y')
+            day += 4;
+
+        // Prompt the user to answer questions
+        System.out.print("\nIs your birthday in Set4?\n");
+        System.out.print(set4);
+        System.out.print("\nEnter 0 for No and 1 for Yes: ");
+        answer = input.next().charAt(0);
+
+        if (answer == 'Y')
+            day += 8;
+
+        // Prompt the user to answer questions
+        System.out.print("\nIs your birthday in Set5?\n");
+        System.out.print(set5);
+        System.out.print("\nEnter 0 for No and 1 for Yes: ");
+        answer = input.next().charAt(0);
+
+        if (answer == 'Y')
+            day += 16;
+
+        System.out.println("\nYour birthday is " + day + "!");
     }
 }
---
+++ src/E4_11.java
@@ -0,0 +1,3 @@
+public class E4_11 {
+    
+}
