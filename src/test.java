7c168733272c6ce6932df1428127688aa1d4de25
i-zanis <55964867+i-zanis@users.noreply.github.com>
2020-06-03 05:51:14 +0100

    3

---
+++ src/test.java
@@ -0,0 +1,7 @@
+public class test {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++)
+            for (int j = 0; j < i; j++)
+                System.out.println(i * j)
+    }
+}
