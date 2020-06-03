aa2f833d9e0c69316528b6f6f84199818c4fdc8f
i-zanis <55964867+i-zanis@users.noreply.github.com>
2020-06-03 22:20:28 +0100

    3

--- src/test.java
+++ src/test.java
@@ -1,7 +1,11 @@
 public class test {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 10; i++) {
+            System.out.println("first");
             for (int j = 0; j < i; j++)
-                System.out.println(i * j)
+                System.out.println(i * j);
+        }
     }
 }
+
+.class 
