79d9a0c5d6d45ed9cffc23aa92103fb1224aac94
i-zanis <55964867+i-zanis@users.noreply.github.com>
2020-06-04 03:16:24 +0100

    3

--- src/test.java
+++ src/test.java
@@ -1,11 +1,13 @@
 public class test {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            System.out.println("first");
-            for (int j = 0; j < i; j++)
-                System.out.println(i * j);
+        double tuition = 10000;
+        double year = 0;
+        double interest = 1.07;
+        while (year !=5) {
+            tuition =  (tuition * interest);
+            year++;
+            System.out.printf("The tuition for %10.2f is %10.2f tuition\n", year, tuition);
         }
+        System.out.println("The tuition is " + tuition);
     }
 }
-
-.class 
