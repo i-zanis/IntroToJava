08bb557defd66041542d3d04661999397bf3fe36
i-zanis <55964867+i-zanis@users.noreply.github.com>
2020-06-09 23:30:04 +0100

    0609

--- src/E4_25.java
+++ src/E4_25.java
@@ -4,10 +4,8 @@ public class E4_25 {
         char plateLetters1 = (char)('A' + Math.random() * 26);
         char plateLetters2 = (char)('A' + Math.random() * 26);
         char plateLetters3 = (char)('A' + Math.random() * 26);
-
-
+        
         int digit = (int)(1000 + Math.random() * (9999-1000));
-
 
         System.out.println("The code you have requested is: \n" + plateLetters1 + "" +  plateLetters2 +  "" + plateLetters3 + digit);
     }
