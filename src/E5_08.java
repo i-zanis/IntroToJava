04f3bbaaf3c0fdf8451dfa35b92fea73d06e5cdd
i-zanis <55964867+i-zanis@users.noreply.github.com>
2020-06-10 23:48:38 +0100

    0610

--- src/E5_08.java
+++ src/E5_08.java
@@ -4,6 +4,7 @@ public class E5_08 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter name and grade separated by space: ");
+
         char k = ' ';
         String student1 = input.next();
         String student2 = input.next();
