3d0ce57a118320cec8e1e676b39381dd195786b0
i-zanis <55964867+i-zanis@users.noreply.github.com>
2020-06-12 04:00:39 +0100

    612

---
+++ src/E5_12.java
@@ -0,0 +1,10 @@
+public class E5_12 {
+    public static void main(String[] args) {
+        int i = 0;
+        int x = 0;
+        while ((int)Math.pow(i,3) <= 12000) {
+            i++;
+        }
+        System.out.println(i);
+    }
+}
