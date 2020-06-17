41adf01f844ec93ee7804529e0c79a35b269d17f
i-zanis <55964867+i-zanis@users.noreply.github.com>
2020-06-17 03:12:57 +0100

    0617312

---
+++ src/E5_24.java
@@ -0,0 +1,13 @@
+public class E5_24 {
+    public static void main(String[] args) {
+        double i = 1.0;
+        double j = 3.0;
+        double sum = 0.0;
+        for (; j < 99.0;) {
+            sum = sum + i/j;
+            i = i + 2;
+            j = j + 2;
+        }
+        System.out.println(sum);
+    }
+}
---
+++ src/E5_25.java
@@ -0,0 +1,20 @@
+public class E5_25 {
+    public static void main(String[] args) {
+
+        double pi = 0;
+
+        for (int i = 1; i <= 100000; i++) {
+            pi += Math.pow(-1, i + 1) / (2 * i - 1);
+            if (i == 10000) {
+                System.out.println("Pi at i = 10000 is " + pi*4);
+            } else if (i == 20000) {
+                System.out.println("Pi at i = 20000 is " + pi*4);
+            } else if (i == 100000) {
+                System.out.println("Pi at i = 100000 is " + pi*4);
+            }
+
+        }
+        System.out.println("Java's pi is " + Math.PI);
+
+    }
+}
