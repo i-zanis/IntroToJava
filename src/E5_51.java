74be3186c1e935b24bd5a821e963dadbe9b6b014
i-zanis <55964867+i-zanis@users.noreply.github.com>
2020-06-20 00:47:14 +0100

    06201247

---
+++ src/E5_51.java
@@ -0,0 +1,13 @@
+public class E5_51 {
+    public static void main(String[] args) {
+        String first = "Welcome to C++";
+        String second = "Welcome to programming";
+        String check = "";
+        for (int i = 0; i < first.length(); i++) {
+            if (first.charAt(i) == second.charAt(i)) {
+                check = check + first.charAt(i);
+            }
+        }
+        System.out.println("The common prefix is: " + check);
+    }
+}
---
+++ src/E6_01.java
@@ -0,0 +1,22 @@
+import sun.awt.windows.WPrinterJob;
+
+import java.sql.SQLOutput;
+
+public class E6_01 {
+    public static void main(String[] args) {
+
+
+        for (int i = 1; i < 100; i++) {
+            System.out.printf("%8d", getPentagonalNumber(i));
+            if (i%10==0) {
+                System.out.println();
+            }
+        }
+
+    }
+
+    public static int getPentagonalNumber(int n) {
+
+        return n * (3 * n - 1) / 2;
+    }
+}
