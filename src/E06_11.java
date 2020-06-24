2b5f724223472a7e030eabe62774c46913d4c349
i-zanis <55964867+i-zanis@users.noreply.github.com>
2020-06-24 19:05:30 +0100

    06240705

--- src/E06_11.java
+++ src/E06_11.java
@@ -1,14 +1,18 @@
 public class E06_11 {
     public static void main(String[] args) {
-
+    computeCommission(10000);
     }
-    public static double computeCommission(double salesAmount) {
-        System.out.printf("%10s, %%10s0", "Sales Amount", "Commission");
-        for (int i = 0; i < 15; i++){
-            System.out.println("-");
+    public static void computeCommission(double salesAmount) {
+
+        System.out.printf("%10s %10s", "Sales Amount", "Commission");
+        System.out.println();
+        for (int i = 0; i < 25; i++){
+            System.out.print("-");
         }
+        System.out.println();
         for (int j = 1; j <=10; j++) {
-            System.out.printf("%6d, %6d", j * 10000, j * 10000* 90/100);
+            System.out.printf("%10.2f %10.2f", j * salesAmount, j * salesAmount * 90/100);
+            System.out.println();
         }
 
  }
