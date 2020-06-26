9ea8db41949f9614b05b345c202627f23d81f5bf
i-zanis <55964867+i-zanis@users.noreply.github.com>
2020-06-26 01:44:33 +0100

    06260144

--- src/E06_30.java
+++ src/E06_30.java
@@ -1,2 +1,28 @@
 public class E06_30 {
+    public static int main(String[] args) {
+        switch (diceRoll() + diceRoll()) {
+            case 2:
+            case 3:
+            case 12:
+                System.out.println("You lose");
+                break;
+            case 7:
+            case 11:
+                System.out.println("Natural, you win!");
+                break;
+            case 4:
+                System.out.println("re-roll");
+                if (diceRoll() + diceRoll() == 4) {
+                    System.out.println("You win!.");
+                }
+                if (diceRoll() + diceRoll() == 7) {
+                    System.out.println("You lose.");
+                } else {
+                }
+
+        }
+        public static int diceRoll() {
+            return (1 + (int) Math.random() * 6);
+        }
+    }
 }
