69eabf6795c5a9192972ce71e9ee872338a67d86
i-zanis <55964867+i-zanis@users.noreply.github.com>
2020-07-04 03:11:57 +0100

    03110704

--- src/E07_16.java
+++ src/E07_16.java
@@ -10,7 +10,7 @@ public class E07_16 {
         int key4 = 75_000_000;
         int key5 = 100_000_000;
         long endTime = 0;
-        long executionTime = 0;
+        double executionTime = 0;
         long startTime = System.nanoTime();
         for (int i = 0; i < bigArray.length; i++) {
             if (bigArray[i] == key1 || bigArray[i] == key2 || bigArray[i] == key3 || bigArray[i] == key4 || bigArray[i] == key5) {
@@ -19,21 +19,22 @@ public class E07_16 {
                 System.out.println("Execution time for " + bigArray[i] + " = " + (executionTime / 100000000.0) + " seconds.");
             }
         }
-        System.out.println(bSearch(75_000_000, bigArray));
+        startTime = System.nanoTime();
+        bSearch(100_000_000, bigArray);
+        endTime = System.nanoTime();
         executionTime = endTime - startTime;
-        System.out.println((executionTime / 100000000.0));
+        System.out.println(executionTime/100000000.0);
     }
 
     public static int bSearch(int key, int[] a) {
-        long startTime = System.nanoTime();
-        return bSearch(key, a, 0, a.length - 1, startTime);
+        return bSearch(key, a, 0, a.length - 1);
     }
 
-    public static int bSearch(int key, int[] a, int low, int hi, long startTime) {
+    public static int bSearch(int key, int[] a, int low, int hi) {
         if (low > hi) return -1;
         int mid = low + (hi - low) / 2;
-        if (key < a[mid]) return bSearch(key, a, low, mid - 1,startTime);
-        else if (key > a[mid]) return bSearch(key, a, mid + 1, hi,startTime);
+        if (key < a[mid]) return bSearch(key, a, low, mid - 1);
+        else if (key > a[mid]) return bSearch(key, a, mid + 1, hi);
         else return mid;
     }
 }
---
+++ src/E07_18.java
@@ -0,0 +1,5 @@
+public class E07_18 {
+    public static void main(String[] args) {
+        
+    }
+}
