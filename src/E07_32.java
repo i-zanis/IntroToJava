5e153f8e15d9fe988ab8688c487155b039a3b2ce
i-zanis <55964867+i-zanis@users.noreply.github.com>
2020-07-08 21:00:42 +0100

    21010907

--- src/E07_32.java
+++ src/E07_32.java
@@ -1,47 +1,57 @@
 public class E07_32 {
-    public static void main(String[] args) {
-        int[] list1 = { 1,12,9,4,4,4,4,5};
-        int[] list2 = {1,3,2,3,4,5};
-        int[] list3 = { 1,2,3,4,5,6,6,6,6};
-        sort(list1, list1[0], list1[list1.length-1]);
-        for (int i =0; i< list1.length; i++) {
-            System.out.println(list1[i]);
+    public static void main (String[] args) {
+        java.util.Scanner input = new java.util.Scanner(System.in);
+
+        // Enter values for list1
+        System.out.print("Enter the list size: ");
+        int size = input.nextInt();
+        int[] list = new int[size];
+        System.out.print("Enter the list content: ");
+        for (int i = 0; i < list.length; i++)
+            list[i] = input.nextInt();
+
+        partition(list);
+
+        System.out.print("After the partition, the list is ");
+        for (int i = 0; i < list.length; i++)
+            System.out.print(list[i] + " ");
+    }
+
+    /** Partition the array list[first..last] */
+    public static int partition(int[] list) {
+        int first = 0;
+        int last = list.length - 1;
+        int pivot = list[first]; // Choose the first element as the pivot
+        int low = first + 1; // Index for forward search
+        int high = last; // Index for backward search
+
+        while (high > low) {
+            // Search forward from left
+            while (low <= high && list[low] <= pivot)
+                low++;
+
+            // Search backward from right
+            while (low <= high && list[high] > pivot)
+                high--;
+
+            // Swap two elements in the list
+            if (high > low) {
+                int temp = list[high];
+                list[high] = list[low];
+                list[low] = temp;
+            }
+        }
+
+        while (high > first && list[high] >= pivot)
+            high--;
+        // Swap pivot with list[high]
+        if (pivot > list[high]) {
+            list[first] = list[high];
+            list[high] = pivot;
+            return high;
+        }
+        else {
+            return first;
         }
     }
-    private static void sort(int[] a, int lo, int hi) {
-        if (hi <= lo) return;
-        int j = partition(a, lo, hi);
-        sort(a, lo, j-1);
-        sort(a, j+1, hi);
-    }
-    private static int partition(int[] a, int lo, int hi) {
-        int i = lo, j = hi + 1;
-        int v = a[lo];
-        while (true) {
-            while ((a[++i] < v)) {
-                if (i == hi) {
-                    break;
-                }
-            }
-
-            while (v < a[--j]) {
-                if (j == lo) {
-                    break;
-                }
-            }
-            if (i >=j) { break; }
-            int temp = i;
-            i = j;
-            j = temp;
-
-
-        }
-            int temp = lo;
-            lo = j;
-            j = temp;
-            return j;
-
-
-    }
-
 }
---
+++ src/E07_33.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class E07_33 {
+    public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+
+            System.out.print("Enter a month: ");
+            int month = input.nextInt();
+            int index = (month - 1  % 13);
+            String[] year = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+            System.out.println(year[index]);
+        }
+    }
