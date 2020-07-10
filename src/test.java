b7d49764f95559f45cb36cf8e28aacb05d1860ae
i-zanis <55964867+i-zanis@users.noreply.github.com>
2020-07-10 22:41:41 +0100

    08371007

--- src/test.java
+++ src/test.java
@@ -1,23 +1,32 @@
 public class test {
 
     public static void main(String[] args) {
-        long sum = 0;
-        long sum1 = 0;
-        double startTime = System.nanoTime();
-        for (int i = 1; i <= 10000; i++) {
-            sum += i;
-        }
-        double endTime = System.nanoTime();
-        System.out.println(("Time " + (endTime - startTime)/100000000));
-        System.out.println(sum);
+       double start = System.nanoTime();
+       abs(5000);
+       double end = System.nanoTime();
+       System.out.println(("manual " + (end-start)));
 
-        startTime = System.nanoTime();
-        sum1 = (10000*(10000+1))/2;
-        endTime = System.nanoTime();
-        System.out.println(("Time " + (double)(endTime - startTime)/100000000));
-        System.out.println(sum1);
+        start = System.nanoTime();
+        Math.abs(5000);
+        end = System.nanoTime();
+        System.out.println("standard " + (end-start));
+
+        start = System.nanoTime();
+        abs(5000);
+        end = System.nanoTime();
+        System.out.println(("manual " + (end-start)));
+
+        start = System.nanoTime();
+        Math.abs(5000);
+        end = System.nanoTime();
+        System.out.println("standard " + (end-start));
 
 
 
+    }
+    public static double abs(double x)
+    {
+        if (x < 0.0) return -x;
+        else return x;
     }
 }
