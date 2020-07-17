79d1e17fd5588a3e4a3e95f8321c8947d8a9f322
i-zanis <ioanlafazanis@gmail.com>
2020-07-17 15:57:48 +0100

    03571707

---
+++ src/E08_16.java
@@ -0,0 +1,21 @@
+public class E08_16 {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {4,2},
+                {1,7},
+                {4,5},
+                {1,2},
+                {1,1},
+                {4,1}};
+    }
+    public static void sort(int m[][]) {
+        for (int i = 0; i < m.length; i++) {
+            int minI = i;
+            for (int j = i + 1; i < m[0].length; j++) {
+                if (m[j][0] < m[minI][0]) { minI = j; }
+                int temp = m[j][0]
+                }
+            }
+        }
+    }
+}
