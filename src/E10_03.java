58ecd1af95b270a707ddfcc8569dfd83bfaf9bc9
i-zanis <ioanlafazanis@gmail.com>
2020-08-03 13:05:43 +0100

    01050803

--- src/E10_03.java
+++ src/E10_03.java
@@ -3,16 +3,20 @@ public class E10_03 {
 
     }
 }
-
 class MyInteger {
+
     private int value;
 
-    MyInteger(int new_value) {
-        value = new_value;
+    public MyInteger(int value){
+        this.value = value;
     }
 
     public int getValue() {
         return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
     }
 
     public boolean isEven() {
@@ -20,25 +24,67 @@ class MyInteger {
     }
 
     public boolean isOdd() {
-        return value % 2 != 0;
+        return !isEven();
     }
 
     public boolean isPrime() {
-        if (value < 2) return false;
-        for (int i = 2; i * i <= value; i++) {
+        double range = Math.sqrt(value);
+        for (int i = 2; i < range; i++) {
             if (value % i == 0) return false;
         }
         return true;
     }
-public static boolean isEven(MyInteger test) {
-    return test.isEven();
-}
-public boolean equals(int new_value) {
-        return value == new_value;
-}
-public boolean equals(MyInteger test, int number)  {
-        return test.getValue() == number;
-}
 
+    public static boolean isEven(int value) {
+        return value % 2 == 0;
+    }
 
+    public static boolean isOdd(int value) {
+        return !isEven(value);
+    }
+
+    public static boolean isPrime(int value) {
+        double range = Math.sqrt(value);
+
+        for (int i = 2; i < range; i++) {
+            if (value % i == 0) return false;
+        }
+        return true;
+    }
+
+    public static boolean isEven(MyInteger myInteger) {
+        return myInteger.isEven();
+    }
+
+    public static boolean isOdd(MyInteger myInteger) {
+        return myInteger.isOdd();
+    }
+
+    public static boolean isPrime(MyInteger myInteger) {
+        return myInteger.isPrime();
+    }
+
+    public boolean equals(int value) {
+        return this.value == value;
+    }
+
+    public boolean equals(MyInteger myInteger) {
+        return this.value == myInteger.value;
+    }
+
+    /** converts an array of numeric characters to an int value. **/
+    public static int parseInt(char[] value) {
+        double vDouble = 0;
+        int toPower = value.length - 1;
+        for (char aValue : value) {
+            vDouble += Math.pow(10, toPower--) * (aValue - '0');
+        }
+
+        return (int)vDouble;
+    }
+
+    public static int parseInt(String value) {
+
+        return MyInteger.parseInt(value.toCharArray());
+    }
 }
