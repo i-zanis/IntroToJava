da41e9621ad0d251f4e4dec3426fca4e236951a2
i-zanis <ioanlafazanis@gmail.com>
2020-08-04 09:32:11 +0100

    09320804

--- src/E10_06.java
+++ src/E10_06.java
@@ -1,16 +1,20 @@
 public class E10_06 {
     public static void main(String[] args) {
-        StackOf_Integers test1 = new StackOf_Integers();
+        StackOf_Integers test1 = new StackOf_Integers(15);
         int count = 120;
-        while (count !=0) {
-            if (isPrime(count)) {test1.push(count);}
+        while (count != 0) {
+            if (isPrime(count)) {
+                test1.push(count);
+            }
             count--;
         }
-        while(test1.isEmpty()) System.out.println(test1.pop() + " ");
+        while (!test1.isEmpty()) {
+            System.out.println(test1.pop() + " ");
+        }
     }
     public static boolean isPrime(int prime) {
         if (prime < 2) return false;
-        for (int i = 2; i * i < prime; i++) {
+        for (int i = 2; i * i <= prime; i++) {
             if(prime % i == 0) return false;
         }
         return true;
