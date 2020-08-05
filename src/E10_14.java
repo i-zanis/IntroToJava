173705a19597934246dbe51a8b7832cad2fd8d31
i-zanis <55964867+i-zanis@users.noreply.github.com>
2020-08-05 06:34:34 +0100

    06340805

---
+++ src/E10_14.java
@@ -0,0 +1,23 @@
+import java.time.Month;
+import java.util.Calendar;
+import java.util.Date;
+
+public class E10_14 {
+    public static void main(String[] args) {
+
+    }
+}
+class MyDate {
+    private int year;
+    private int month;
+    private int day;
+
+    MyDate() {
+        year = Calendar.YEAR;
+        month = Calendar.MONTH;
+        day = Calendar.DAY_OF_MONTH;
+    }
+    MyDate(long elapsedTime) {
+        
+    }
+}
