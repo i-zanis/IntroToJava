aac9d6c01e7c168a4ab340fde684e4ce45e37466
i-zanis <ioanlafazanis@gmail.com>
2020-08-07 19:23:16 +0100

    07230807

--- src/test.java
+++ src/test.java
@@ -1,6 +1,6 @@
 public class test {
     public static void main(String[] args) {
-        for (int i = 2; i * i < 101; i ++) System.out.println(i);
+        for (int i = 2; i * i < 500; i ++) System.out.println(i);
 
         
     }
