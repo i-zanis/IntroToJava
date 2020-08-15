bd3d6defb8e1d043d0093c397e6b67813d9b1583
i-zanis <ioanlafazanis@gmail.com>
2020-08-15 01:26:48 +0100

    01260815

---
+++ src/ComputeFactorialTailRecursion.java
@@ -0,0 +1,9 @@
+public class ComputeFactorialTailRecursion {
+    public static long factorial(int n) {
+            return factorial(n, 1); // Call auxiliary method
+        }/* Auxiliary tail−recursive method for factorial*/
+        private static long factorial(int n, int result) {
+            if (n == 0) return result;
+            else return factorial(n -1, n * result);//Recursive call
+        }
+    }
--- src/test.java
+++
@@ -1,9 +0,0 @@
-public class test {
-    public static void main(String[] args) {
-        for (int i = 2; i * i < 500; i ++) System.out.println(i);
-
-        
-    }
-
-}
-
