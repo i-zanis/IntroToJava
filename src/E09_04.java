8765691bc78415e1fc316731b4acc3f67bdeeb1e
i-zanis <55964867+i-zanis@users.noreply.github.com>
2020-09-05 22:32:58 +0100

    223209052020

--- src/E09_04.java
+++ src/E09_04.java
@@ -3,7 +3,6 @@ import java.util.Random;
 public class E09_04 {
     public static void main(String[] args) {
 
-
     Random random = new Random(1000);
     for (int i = 0; i < 50; i++) {
         System.out.println(random.nextInt(100));
