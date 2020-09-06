a3ee60b07a27df5df3d5112f4a1c6337ee276906
i-zanis <55964867+i-zanis@users.noreply.github.com>
2020-09-06 04:37:55 +0100

    03370906

--- src/E11_19.java
+++ src/E11_19.java
@@ -1,10 +1,33 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class E11_19 {
     public static void main(String[] args) {
+        @SuppressWarnings("resource")
         Scanner input = new Scanner(System.in);
-        int count = input.nextInt();
-        
-
+        System.out.print("Enter the number of objects: ");
+        int numberOfObjects = input.nextInt();
+        ArrayList<Integer> objects = new ArrayList<>();
+        System.out.print("Enter the weights of the objects: ");
+        for (int i = 0; i < numberOfObjects; i++) {
+            objects.add(input.nextInt());
+        }
+        int container = 1;
+        while(!objects.isEmpty()) {
+            System.out.println("Container " + container++ + " contains objects with weight " + getConteiner(objects, 10));
+        }
     }
+    static String getConteiner(ArrayList<Integer> objects, int max) {
+        String result = "";
+        for (int i = 0; i < objects.size(); i++) {
+            if(objects.get(i) <= max) {
+                int tmp = objects.get(i);
+                result += tmp + " ";
+                objects.remove(i);
+                return result + getConteiner(objects, max - tmp);
+            }
+        }
+        return result;
+    }
+
 }
