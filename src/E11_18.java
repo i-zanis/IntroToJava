cbd2ac2d7de5fb412b4e37fc03bfc027ea9cabaf
i-zanis <55964867+i-zanis@users.noreply.github.com>
2020-09-06 01:56:24 +0100

    01560906

--- src/E11_18.java
+++ src/E11_18.java
@@ -2,12 +2,9 @@ import java.util.ArrayList;
 
 public class E11_18 {
     public static void main(String[] args) {
-    String test = "yahoo";
-    ArrayList<Character> s =  new ArrayList<>();
-    s = toCharacterArray(test);
-    for (Character e : s) {
-        System.out.println(e);
-    }
+        for (Character c : toCharacterArray("Welcome")) {
+            System.out.print(c);
+        }
     }
     public static ArrayList<Character> toCharacterArray(String s) {
         ArrayList<Character> list = new ArrayList<>();
---
+++ src/E11_19.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class E11_19 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int count = input.nextInt();
+        
+
+    }
+}
