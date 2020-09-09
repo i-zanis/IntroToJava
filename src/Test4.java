a75b014317c69ec2484aa56973b7ecff56e3c125
i-zanis <ioanlafazanis@gmail.com>
2020-09-09 02:01:45 +0100

    02010909

--- src/Test4.java
+++ src/Test4.java
@@ -1,33 +1,18 @@
 public class Test4 {
     public static void main(String[] args) {
+        public E removeFirst() {
+            if (size == 0) return null; // Nothing to delete
+            else {
+                Node<E> temp = head; // Keep the first node temporarily
+                head = head.next; // Move head to point to next node
+                size--; // Reduce size by 1
+                if (head == null) tail = null; // List becomes empty
+                return temp.element; // Return the deleted element
+            }
     }
-    fsfsdfsfdsf
-    fsfsdfsfdsf
-    public void add(int index, E e) {
-        if (index == 0) addFirst(e); // Insert first
-        else if (index >= size) addLast(e); // Insert last
-        else{ // Insert in the middle
-            Node<E> current = head;
-            for (int i = 1; i < index; i++)
-                current = current.next;
-            Node<E> temp = current.next;
-            current.next = new Node<>(e);
-            (current.next).next = temp;
-            size++;
-        }
-        }
-         }
 
-    public void add(int index, E e) {
-        if (index == 0) addFirst(e); // Insert first
-        else if (index >= size) addLast(e); // Insert last
-        else{ // Insert in the middle
-            Node<E> current = head;
-            for (int i = 1; i < index; i++)
-                current = current.next;
-            Node<E> temp = current.next;
-            current.next = new Node<>(e);
-            (current.next).next = temp;
-            size++;
-        }
+}
+
+
+
 
