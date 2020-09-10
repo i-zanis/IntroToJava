cf22e7f2ae71641973051846cabba446f66f87e9
i-zanis <ioanlafazanis@gmail.com>
2020-09-10 17:19:49 +0100

    09102020

--- src/E15.java
+++ src/E15.java
@@ -1,27 +1,16 @@
-public boolean insert(E e) {
-    if (root == null) root = createNewNode(e); // Create a new root
-    else {
-         TreeNode<E> parent = null;
-         TreeNode<E> current = root;
-         while (current != null)
-         if (e.compareTo(current.element) < ) {
-         parent = current;
-         current = current.left;
-         }
-         else if (e.compareTo(current.element) > ) {
-         parent = current;
-         current = current.right;}
-
-         else
-         return false; // Duplicate node not inserted
-
-         // Create the new node and attach it to the parent node
-         if (e.compareTo(parent.element) < )
-         parent.left = createNewNode(e);
-         else
-         parent.right = createNewNode(e);
-         }
-
-         size++;
-         return true; // Element inserted successfully
-         }
+public boolean delete(E e) {
+        155 // Locate the node to be deleted and also locate its parent node
+        156 TreeNode<E> parent = null;
+        157 TreeNode<E> current = root;
+        158 while (current != null) {
+        159 if (e.compareTo(current.element) < 0) {
+        160 parent = current;
+        161 current = current.left;
+        162 }
+        163 else if (e.compareTo(current.element) > 0) {
+        164 parent = current;
+        165 current = current.right;
+        166 }
+        167 else
+        168 break; // Element is in the tree pointed at by current
+        169 }
