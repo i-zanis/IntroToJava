8a3bb885eb5168860c4abdacab89f066d3912acd
i-zanis <ioanlafazanis@gmail.com>
2020-09-10 19:07:21 +0100

    07070910

---
+++ src/E12_4.java
@@ -0,0 +1,10 @@
+public class E12_4 {
+    public static void main(String[] args) {
+        try { Loan loan = new Loan(2, 0, -10) {
+            catch(
+            IllegalArgumentException ex)
+
+            {
+                StringIndexOutOfBoundsException("IllegalArgumentException: " + ex.getMessage());
+            }
+            class Loan{}
