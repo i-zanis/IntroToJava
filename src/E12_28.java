03a57fa6de7ee2c29e9e39f5956d77ea5ca9f40e
i-zanis <55964867+i-zanis@users.noreply.github.com>
2020-09-23 02:31:03 +0100

    09230231

---
+++ src/E12_28.java
@@ -0,0 +1,21 @@
+public class E12_28 {
+    public static void main(String[] args) {
+        if(args.length == 0) {
+            System.out.println("Usage: java E12_28");
+            System.exit(1);
+        }
+        StringBuilder sb = new StringBuilder();
+        String s =
+        matches("Exercise[\\d]_[\\d]") {
+            sb.insert(s.indexOf('_' -1, "0")))
+        }
+        sb.renameTo
+    }
+}
+/**(Rename files) Suppose you have a lot of files in a directory named Exercisei_j,
+ where i and j are digits. Write a program that pads a 0 before i if i is a single
+ digit. For example, a file named Exercise2_1 in a directory will be renamed to
+ Exercise02_1. In Java, when you pass the symbol * from the command line,
+ it refers to all files in the directory (see Supplement III.V). Use the following
+ command to run your program:
+ java Exercise12_28 **/
--- src/Etest12.java
+++ src/Etest12.java
@@ -1,4 +1,13 @@
-import java.util.Collections;
-public class Etest12 {
 
+public class Etest12 {
+    public static void main(String[] args) {
+
+
+        String s = "Exercise2_7";
+        System.out.println(s.length());
+        System.out.println(s.indexOf('_')-1);
+        System.out.println(s.indexOf('_')+2);
+
+
+    }
 }
