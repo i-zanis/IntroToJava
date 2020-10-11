76d2139bd4c9d4666fa291bc1cdfbfb9c735e04d
i-zanis <55964867+i-zanis@users.noreply.github.com>
2020-10-11 04:02:03 +0100

    04011110

---
+++ src/rwerwre.java
@@ -0,0 +1,12 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class rwerwre {
+    public static void main(String[] args) {
+       int yoNigga = 10;
+       String[] stringArray = new String[5];
+       stringArray[1] = yoNigga;
+
+
+    }
+}
