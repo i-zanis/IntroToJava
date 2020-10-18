b53f51d86be5ba9dfe062699cb6a9b0eec36567b
i-zanis <55964867+i-zanis@users.noreply.github.com>
2020-10-18 05:41:42 +0100

    18102020

---
+++ src/AmericanFlag.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+/** Student Name: Your mother is my new bike.
+ * Student ID: 666
+ * Course: I thought this was computer science
+ */
+public class AmericanFlag {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Who is the President of United States?");
+        String input = scan.nextLine();
+        if (input.equalsIgnoreCase("Trump")){
+            System.out.println("Correct! Make programming great again!");
+
+            System.out.println("  O");
+            System.out.println(" | |");
+            System.out.println(" | |\u001B[30m\u001b[41;1m\u001b[44;1m * * * * * * \u001B[30m \u001b[41;1m=========================\033[0m");
+            System.out.println(" | |\u001B[30m\u001b[41;1m\u001b[44;1m  * * * * * * \u001B[30m\u001b[41;1m=========================\033[0m");
+            System.out.println(" | |\u001B[30m\u001b[41;1m\u001b[44;1m * * * * * *  \u001B[30m\u001b[41;1m=========================\033[0m");
+            System.out.println(" | |\u001B[30m\u001b[41;1m\u001b[44;1m  * * * * * * \u001B[30m\u001b[41;1m=========================\033[0m");
+            System.out.println(" | |\u001B[30m\u001b[41;1m\u001b[44;1m * * * * * *  \u001B[30m\u001b[41;1m=========================\033[0m");
+            System.out.println(" | |\u001B[30m\u001b[41;1m\u001b[44;1m  * * * * * * \u001B[30m\u001b[41;1m=========================\033[0m");
+            System.out.println(" | |\u001B[30m\u001b[41;1m\u001b[44;1m * * * * * *  \u001B[30m\u001b[41;1m=========================\033[0m");
+            System.out.println(" | |\u001B[30m\u001b[41;1m\u001b[44;1m  * * * * * * \u001B[30m\u001b[41;1m=========================\033[0m");
+            System.out.println(" | |\u001B[30m\u001b[41;1m\u001b[44;1m * * * * * *  \u001B[30m\u001b[41;1m=========================\033[0m");
+            System.out.println(" | |\u001B[30m\u001b[41;1m=======================================\033[0m");
+            System.out.println(" | |\u001B[30m\u001b[41;1m=======================================\033[0m");
+            System.out.println(" | |\u001B[30m\u001b[41;1m=======================================\033[0m");
+            System.out.println(" | |\u001B[30m\u001b[41;1m=======================================\033[0m");
+            System.out.println(" | |\u001B[30m\u001b[41;1m=======================================\033[0m");
+            System.out.println(" | |\u001B[30m\u001b[41;1m=======================================\033[0m");
+            System.out.println(" | |");
+            System.out.println(" | |");
+            System.out.println(" | |");
+            System.out.println(" | |");
+            System.out.println(" | |");
+            System.out.println(" | |");
+            System.out.println(" | |");
+            System.out.println(" | |");
+            System.out.println(" | |");
+            System.out.println(" | |");
+            System.out.println(" | |");
+            System.out.println(" | |");
+            System.out.println(" | |");
+            System.out.println(" ---");
+
+        }
+        else {
+            System.out.println("Incorrect! Guess again");
+        }
+    }
+}
