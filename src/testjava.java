3c24b74f6260750b97270445a6020012d64877b2
i-zanis <ioanlafazanis@gmail.com>
2020-11-27 23:51:48 +0000

    11271511

--- src/testjava.java
+++ src/testjava.java
@@ -1,10 +1,5 @@
 public class testjava {
     public static void main(String[] args) {
-        System.out.println(SongDecoder("WUBWUBABCWUB"));
-    }
-
-    public static String SongDecoder(String song) {
-        String yo = song.replace("WUB", "");
-            return yo;
+        System.out.println("yo");
     }
 }
