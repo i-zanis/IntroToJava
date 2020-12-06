ae32d0b13be364fb8aee7f3e77b874574fdeffce
i-zanis <55964867+i-zanis@users.noreply.github.com>
2020-12-06 23:33:57 +0000

    23331606

--- src/test11.java
+++ src/test11.java
@@ -1,11 +1,15 @@
-public class test11 {
-    public static void main(String args[]) throws Exception {
-        displayItems();
-        }
-        public void displayItems() {
-            System.out.println("Peanuts press [A]");
-            System.out.println("Apples press [B]");
-            System.out.println("Mandarin press [C]");
-            System.out.println("Rottem Shit press [D]");
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class test111 {
+    class Solution {
+        public int singleNumber(int[] nums) {
+            if (nums.length == 0) return 0;
+            Arrays.sort(nums);
+            for (int i = 1; i < nums.length - 2; i++) {
+                if (nums[i] != nums[i + 1] && nums[i] != nums[i + 1]) return nums[i];
+                else return nums[length -2 ];
+            }
+            return nums[0];
         }
     }
